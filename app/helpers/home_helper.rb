b8d03773d5f8ea640660fddcb63b4408b9361ae3
Arthur Poulet <arthur.poulet@mailoo.org>
2017-05-03 00:39:49 +0100

    Add some grey-blue color to readme, tags, ...

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,7 @@
 module HomeHelper
   def tag_list_links(post)
     post.tag_list.map do |tag|
-      link_to tag, root_path(tag: tag), class: "label label-default"
+      link_to tag, root_path(tag: tag), class: "label"
     end.join(" ").html_safe
   end
 end
